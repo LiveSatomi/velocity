8272812d7d75362f5405b75918348359f9918c92
Jack Mueller <mueller.o.jack@gmail.com>
2020-06-28 12:41:47 -0400

    Add documentation to the ShipDriver class.

--- Assets/Scripts/Ship/ShipDriver.cs
+++ Assets/Scripts/Ship/ShipDriver.cs
@@ -7,36 +7,72 @@ namespace Ship {
     public class ShipDriver : MonoBehaviour {
         public delegate void CourseFinishedEvent();
 
+        /// <summary>
+        ///     Cached animator parameter.
+        /// </summary>
         private static readonly int AnimatorChangeDirection = Animator.StringToHash("changeDirection");
 
+        /// <summary>
+        ///     Speed boost based on performance. Decays to 0 over time.
+        /// </summary>
         private readonly float speedBoost = 1;
 
+        /// <summary>
+        ///     Cached animator component.
+        /// </summary>
         private Animator animator;
 
+        /// <summary>
+        ///     Indicates the percentage of lane change animation completed. Controlled by animator.
+        /// </summary>
         public float changeProgress;
 
+        /// <summary>
+        ///     State that represents if the ship has collided with an obstacle.
+        /// </summary>
         private bool collided;
 
+        /// <summary>
+        ///     Unity InputAction that controls the ship.
+        /// </summary>
         private ShipInputAction inputAction;
 
         // TODO Initialize this from TrackBuilder or elsewhere
         public float laneWidth = 2;
 
+        /// <summary>
+        ///     Cached rigidbody component.
+        /// </summary>
         private Rigidbody rb;
 
+        /// <summary>
+        ///     Authority for elapsed time.
+        /// </summary>
         public TimeController timeController;
 
+        /// <summary>
+        ///     State tied to the desired direction to change lanes.
+        /// </summary>
         public float InputDirection { get; private set; }
 
+        /// <summary>
+        ///     State that represents the direction of the lane change being executed.
+        /// </summary>
         public float ChangeDirection { get; set; }
 
+        /// <summary>
+        ///     State that tells where the animation started. Used to adjust for rounding errors at the end of each lane change.
+        /// </summary>
         public float ChangeStartPosition { get; set; }
 
+        /// <summary>
+        ///     Current forward velocity.
+        /// </summary>
         public float Speed { get; private set; }
 
         public static event CourseFinishedEvent OnCourseFinished;
 
-        private void Awake() {
+        void Awake() {
             inputAction = new ShipInputAction();
             inputAction.ShipControls.ChangeLane.performed += ctx => {
                 InputDirection = Math.Sign(ctx.ReadValue<float>());
@@ -44,20 +80,20 @@ namespace Ship {
         }
 
         // Start is called before the first frame update
-        private void Start() {
+        void Start() {
             rb = GetComponent<Rigidbody>();
             animator = GetComponent<Animator>();
         }
 
         // Update is called once per frame
-        private void Update() {
+        void Update() {
             if (!collided) {
                 animator.SetFloat(AnimatorChangeDirection, InputDirection);
                 Speed = timeController.CurrentMinSpeed() + speedBoost;
             }
         }
 
-        private void FixedUpdate() {
+        void FixedUpdate() {
             var trans = transform;
             var positionNow = trans.position;
             if (Math.Abs(ChangeDirection) > .01)
@@ -68,11 +104,11 @@ namespace Ship {
         }
 
 
-        private void OnEnable() {
+        void OnEnable() {
             inputAction.Enable();
         }
 
-        private void OnDisable() {
+        void OnDisable() {
             inputAction.Disable();
         }
 
